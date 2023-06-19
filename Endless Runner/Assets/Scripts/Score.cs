c37db9ded5884df5fbeadb669385851db8dec8da
Matthew Wierenga <matt.wierenga@outlook.com>
2023-06-19 14:31:52 +0200

    Added music to the game

--- Endless Runner/Assets/Scripts/Score.cs
+++ Endless Runner/Assets/Scripts/Score.cs
@@ -7,16 +7,20 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public  int coin = 0;
+    public AudioClip audioSource;
 
     public TextMeshProUGUI coinText;
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Coin")
         {
+            
             coin++;
+            AudioSource.PlayClipAtPoint(audioSource, transform.position);
             coinText.text = "Coin: " + coin.ToString();
             Debug.Log(coin);
             Destroy(other.gameObject);
+
         }
     }
 }
